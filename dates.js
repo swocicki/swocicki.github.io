ca4968cbdc91987f891884c2d274d27cedf2971b
swocicki22 <swocicki@gmail.com>
2022-03-04 12:27:58 -0700

    update

--- dates.js
+++ dates.js
@@ -1,3 +1,5 @@
+/* This should work....I hope */
+
 document.querySelector("#year").textContent = new Date().getFullYear();
 
 document.getElementById("lastModif").textContent = `Last Updated:  ${document.lastModified}`;
